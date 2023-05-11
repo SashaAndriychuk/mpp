b410703701ca3dbf6883f5d9c6d7c8a5f77a3ecd
SashaAndriychuk <91837549+SashaAndriychuk@users.noreply.github.com>
2023-05-11 16:52:59 +0300

    Delete hospital1 directory

--- hospital1/db/migrate/20230326121530_create_clinics.rb
+++
@@ -1,9 +0,0 @@
-class CreateClinics < ActiveRecord::Migration[7.0]
-  def change
-    create_table :clinics do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
--- hospital1/db/migrate/20230326121730_create_pacients.rb
+++
@@ -1,8 +0,0 @@
-class CreatePacients < ActiveRecord::Migration[7.0]
-  def change
-    create_table :pacients do |t|
-      t.string :fio
-      t.timestamps
-    end
-  end
-end
